4800ef9f7884a8503876e03581c73fcb3dfffa13
sging <rhrnrkd135@gmail.com>
2024-06-07 23:58:06 +0900

    + 설계 commit

---
+++ js/test.js
@@ -0,0 +1,31 @@
+
+/**
+ * 월 배당 계산기
+ * 환율 연동
+ * 수수료 계산
+ * Add, delete, lender, update
+ */
+
+// 환율 정보 조회
+const getExchangeRate = async () => {
+    // const { result } = await getExchangeRateApi();
+    // if (!result) {
+    //   return;
+    // }
+  };
+  
+  // 유저 목록 조회
+const getTradeItemList = async () => {
+    // const { result } = await getTradeItemListApi();
+    // if (!result) {
+    //   return;
+    // }
+  };
+  
+  //  const mockData = [
+  //   {
+  //     id:0,
+  //     name:'SCHD',
+  //     stockCount:30,
+  //     dividends: 1,
+  //     currentPrice:10800
